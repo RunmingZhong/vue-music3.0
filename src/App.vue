191860e0a03d1252f54b8f61eb16266819915b6a
RunmingZhong <33444272+RunmingZhong@users.noreply.github.com>
2022-05-05 13:30:59 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <m-header></m-header>
 </template>
+
+<script>
+  import Header from '@/components/header/header'
+  export default {
+    components:{
+      MHheader:Header
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
